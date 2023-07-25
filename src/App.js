6c010da85046a30df50bbdafdbfe57f558fb0357
Doritao <carloshenriqueneves6@gmail.com>
2023-07-25 00:01:58 -0300

    usando useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef , useEffect} from "react";
 import axios from "axios";
 import BurgerIMG from "./Assets/burger 1.svg";
 import Trash from "./Assets/trash.svg";
@@ -19,34 +19,42 @@ import {
 
 const App = () => {
   const [orders, setOrders] = useState([]);
-  const inputOrder = useRef()
-  const inputClientName = useRef()
-
+  const inputOrder = useRef();
+  const inputClientName = useRef();
 
   async function newOrder() {
-     setOrders([...orders,{id: Math.random(), clientOrder: inputOrder.current.value, clientName: inputClientName.current.value}])
-      const newOrderData  = {
-        order: inputOrder.current.value,
-        clientName: inputClientName.current.value
-      }
-     const data = await axios.post('http://localhost:3001/orders',newOrderData)
+    const { data: newOrderData } = await axios.post("http://localhost:3001/orders", {
+      order: inputOrder.current.value,
+      clientName: inputClientName.current.value,
+    });
+    setOrders((oldState) => [...oldState, newOrderData]);
+    console.log(newOrderData);
   }
-  
-function deleteOrder(orderID) {
-  const newArrayOrders = orders.filter((order) => order.id !== orderID);
 
-  setOrders(newArrayOrders)
-}
+  useEffect(() => {
+    async function fetchOrders() {
+      const { data: newORderData} = await axios.get('http://localhost:3001/orders')
+      setOrders(newORderData)
+    }//o use effect nao aceita async portanto basta criar uma funcao dentro dele para usaar o async
+    fetchOrders()
+  },[])
+//useEffect vai ser chamado em duas ocasioes
+//quando a pagina carregar
+//quando o estado orders for alterado
+
+
+  function deleteOrder(orderID) {
+    const newArrayOrders = orders.filter((order) => order.id !== orderID);
+
+    setOrders(newArrayOrders);
+  }
   return (
     <Container>
       <Img alt="burger-logo" src={BurgerIMG} />
       <H1>Faça seu pedido!</H1>
       <ContainerItens>
         <InputLabel>Pedido</InputLabel>
-        <Input
-          placeholder="1 Coca-Cola, 1-X Salada"
-          ref={inputOrder}
-        ></Input>
+        <Input placeholder="1 Coca-Cola, 1-X Salada" ref={inputOrder}></Input>
         <InputLabel>Nome do Cliente</InputLabel>
         <Input placeholder="Steve Jobs" ref={inputClientName}></Input>
         <Button onClick={newOrder}>Novo Pedido</Button>
