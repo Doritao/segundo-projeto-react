80502faa1769db7b4b21fc311efefd8d4eb5cd28
Doritao <carloshenriqueneves6@gmail.com>
2023-07-26 21:11:58 -0300

    usando spread operator

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,7 +2,7 @@ import react from 'react'
 import {Button as ButtonComponent} from './styled'
 
 
-function Button({onClick,children,isBlack}) { 
+function Button({children,...props}) { 
     return( <ButtonComponent onClick={onClick} isBlack={isBlack}>{children}</ButtonComponent>)
 }
 
